6e80510f3023ab152936062a54029d7e77c30506
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2023-04-09 15:11:46 +0700

    update

--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -13,6 +13,28 @@ class MongoDB {
         });
         return this.client;
     }
+
+    static async disconnect() {
+        if (!this.client) {
+            return;
+        }
+        await this.client.close();
+        this.client = null;
+    }
+
+    static async getDatabase(database) {
+        if (!this.client) {
+            throw new Error('MongoDB client is not connected');
+        }
+        return this.client.db(database);
+    }
+
+    static async getCollection(database, collection) {
+        if (!this.client) {
+            throw new Error('MongoDB client is not connected');
+        }
+        return this.client.db(database).collection(collection);
+    }
 }
 
 
--- server.js
+++ server.js
@@ -1,39 +1,45 @@
-import express from 'express'
-import {print, type} from './app/helpers/print.js'
 import * as dotenv from 'dotenv'
 dotenv.config()
+
+import express from 'express'
+import {print, type} from './app/helpers/print.js'
 import { usersRouter, studentsRouter } from './app/routes/index.js'
 import MongoDB from './app/utils/mongodb.util.js'
 import { Exception }  from './app/errors/index.js'
 
 import checkToken from './app/middlewares/auth.js'
 
-const app = express()
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(checkToken)
 app.use(express.json())
-
-const PORT = process.env.PORT || 3000
 
 // routers
 app.use('/users', usersRouter)
 app.use('/students', studentsRouter)
 
+const handleMongoDBError = (error) => {
+  const { code } = error;
+  if (code == 8000) {
+      throw new Exception(Exception.DB_USERNAME_PASSWORD);
+  } else if (code == 'ENOTFOUND') {
+      throw new Exception(Exception.DB_CONNECTION_STRING);
+  }
+  throw new Exception(Exception.DB_CONNECTION);
+};
+
 const startServer = async() => {
   try {
-      await MongoDB.connect(process.env.MONGODB_URI);
-      print('Connected to MongoDB', type.SUCCESS)
+      await MongoDB.connect(process.env.MONGODB_URI, () => {
+        print('Connected to MongoDB', type.SUCCESS)
+      })
       
       app.listen(PORT, () => {
-          print(`Server is running on port ${PORT}`, type.SUCCESS)
+        print(`Server is running on port ${PORT}`, type.SUCCESS)
       })
   } catch (error) {
-      const {code} = error
-      if(code == 8000){
-        throw new Exception(Exception.DB_USERNAME_PASSWORD)
-      } else if(code == 'ENOTFOUND'){
-        throw new Exception(Exception.DB_CONNECTION_STRING)
-      }
-      throw new Exception(Exception.DB_CONNECTION)
+      handleMongoDBError(error);
   }
 }
 
